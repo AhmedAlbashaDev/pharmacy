21d80cb2a2c054edaca14a648938c1f1ba98c776
Fox-Ahmed <foxa233@gmail.com>
2020-02-12 01:06:57 +0200

    'add_edits'

--- lib/Pages/PdminCall.dart
+++ lib/Pages/PdminCall.dart
@@ -8,96 +8,69 @@ class Acall extends StatelessWidget {
       child: Container(
         child: Scaffold(
           appBar: AppBar(
-            title: Text('اتصل بنا'),
+            title: Text('اتــصــل بـنا'),
           ),
           body: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
+              SizedBox(
+                height: 80,
+              ),
               Container(
-                  padding: new EdgeInsets.all(33.0),
-                  margin: EdgeInsets.only(
-                    right: 120,
-
-                  ),
-                  child: Row(
-
+                width: 120.0,
+                height: 120.0,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    image: new DecorationImage(
+                        image: new ExactAssetImage('lib/assets/logo.png'),
+                        fit: BoxFit.cover),
+                    border: Border.all(color: Colors.blue, width: 5.0),
+                    borderRadius:
+                    new BorderRadius.all(const Radius.circular(80.0))),
+              ), //______________Logo______________
+              SizedBox(
+                height: 100,
+              ), //=====
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
-                      CircleAvatar(
-                        radius: 60.0,
-                        backgroundImage: AssetImage('lib/assets/logo.png'),
-                      ),
+                      Text('هواتفنا',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
+                      Column(
+                        children: <Widget>[
+                          Text('00249909040353'),
+                          SizedBox(height: 10,),
+                          Text('00249960605023'),
+                          SizedBox(height: 10,),
+                          Text('00249116966864'),
+                        ],
+                      )
+                    ],
+                  ),
+
+                  SizedBox(height: 20,),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Text('الايميل',style: TextStyle(fontSize: 18,fontWeight: FontWeight.w500),),
+                      Column(
+                        children: <Widget>[
+                          Text('mohammed98@gmail.com'),
+
+                        ],
+                      )
                     ],
                   )
-                  ),//__________LogoApp____________
-                  SizedBox(
-                    height: 130,
-                  ),
-              Padding(
-                padding: EdgeInsets.only(
-                  left: 40,
-                  right: 40,
-                ),
-                child: Container(
-                  width: double.maxFinite,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.lightBlue),
-                    borderRadius: BorderRadius.all(Radius.circular(40)),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.only(right: 30, left: 40, top: 50),
-                    child: TextField(
-                      decoration: InputDecoration(
-                          hintText: 'اكتـب تعليــق...',
-                          labelStyle: TextStyle(color: Colors.grey),
-                          border:
-                              UnderlineInputBorder(borderSide: BorderSide.none),
-                          icon: Icon(
-                            Icons.comment,
-                            color: Colors.blue,
-                          )),
-                    ),
-                  ),
-                ),
-              ),//_________________________
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                margin: EdgeInsets.all(30),
-                width: 130,
-                height: 40,
-                child: RaisedButton(
-                  shape: new RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(20),
-                      side: BorderSide(color: Colors.blue)),
-                  onPressed: () {},
-                  color: Colors.blue,
-                  textColor: Colors.white,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: <Widget>[
-                      Padding(
-                        padding: EdgeInsets.only(left: 10, right: 15),
-                        child: Icon(Icons.send),
-                      ),
-                      Padding(
-                          padding: EdgeInsets.only(top: 4),
-                          child: Text(
-                            'ارســال',
-                            style: TextStyle(fontSize: 18),
-                          ))
-//
-                    ],
-                  ),
-
-//
-                ),//________________________________
-              ),
+                ],
+              ), //_________________________________
             ],
-          ),//
+          ),
         ),
       ),
-    );//____________Translate_To_Arabic____________
+    ); ///____________Translate_To_Arabic____________
   }
 }
